411da3ded2c3241a1347a841ab253b3e1fe496d2
EndoTsuyoshi <endotsuyoshi@EndoTsuyoshi-no-MacBook-Pro.local>
2014-05-11 12:34:49 +0900

    20140511

--- Shiroitoko/SHViewController.m
+++ Shiroitoko/SHViewController.m
@@ -17,6 +17,7 @@
 NSMutableArray *arrTile;
 int widthTile;
 int heightTile;
+int widthLine;
 
 int totalColumnNum;//列数
 int totalRowNum;//行数
@@ -28,16 +29,20 @@ int totalRowNum;//行数
 	// Do any additional setup after loading the view, typically from a nib.
     
     
-    self.view.backgroundColor = [UIColor orangeColor];
+    self.view.backgroundColor = [UIColor blackColor];
     arrTile = [NSMutableArray array];
     
     int x0 = 0;
     int y0 = 0;
     
-    widthTile = self.view.bounds.size.width/4;
-    heightTile = self.view.bounds.size.height/4;
+    
     totalColumnNum = 4;
     totalRowNum = 4;
+    
+    
+    widthLine = 3;//px
+    widthTile = self.view.bounds.size.width/4-widthLine;
+    heightTile = self.view.bounds.size.height/4-widthLine;
     
     NSMutableArray *arrTmpTile = [NSMutableArray array];
     BOOL isAlreadyOn = false;//既にオフのブロックがないか(一列の中でどれかはオンにならなければならない)
@@ -45,14 +50,18 @@ int totalRowNum;//行数
         arrTmpTile = [NSMutableArray array];
         isAlreadyOn = NO;
         for(int i = 0; i < totalColumnNum;i ++){
-            x0 = i * widthTile;
-            y0 = j * heightTile;
+            x0 = i * (widthTile + widthLine);
+            y0 = j * (heightTile + widthLine);
             SHTile *myTile = [[SHTile alloc]initWithFrame:CGRectMake(x0, y0, widthTile, heightTile)];
-            if(arc4random() % totalColumnNum == 0 && !isAlreadyOn){
+            myTile.viewTile.tag = [[NSString stringWithFormat:@"%d%d", j,i] intValue];//j行i列をjiとタグ付けする
+//            UITapGestureRecognizer *gesture =
+//            [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapAction:)];
+//            [myTile.viewTile addGestureRecognizer:gesture];
+            if(arc4random() % (totalColumnNum-1) == 0 && !isAlreadyOn){
                 [myTile changeColor];
                 isAlreadyOn = YES;
                 NSLog(@"%d列目の%d行目がオン", j,i);
-            }else if(i == totalColumnNum - 1){
+            }else if(i == totalColumnNum - 1 && !isAlreadyOn){
                 [myTile changeColor];
                 isAlreadyOn = YES;//あまり意味ないけど
                 NSLog(@"%d列目の%d行目がオン", j,i);
@@ -79,6 +88,11 @@ int totalRowNum;//行数
             NSLog(@"view[%d][%d] = %@", j,i,((SHTile *)arrTile[j][i]).viewTile);
         }
     }
+}
+
+-(void)tapAction:(id)sender{
+    
+    
 }
 
 @end
